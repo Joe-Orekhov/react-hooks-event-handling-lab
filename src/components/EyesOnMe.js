691917f1cf64d5686e2561817caeb18456bc3d8e
joesephorekhov <josephorekhov@gmail.com>
2021-09-02 09:38:07 -0600

    DONE

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
-// Code EyesOnMe Component Here
+import { useState } from "react";
+
+
+function EyesOnMe(){
+  const [ Good, setGood ] = useState(true);
+  const [ eyesOn, setEyesOn ] = useState(true);
+  function good(){
+    console.log('Good!')
+    
+  }
+  function onBlur(){
+    console.log('Hey! Eyes on me!')
+    
+  }
+  return(
+    <div>
+      <button onBlur={onBlur} onFocus={good} >Eyes on me</button>
+    </div>
+  )
+};
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
-// Code Keypad Component Here
+import { useState } from "react";
+
+
+function Keypad(){
+  const [ noinput, input] = useState(" ")
+function LoadingChange(){
+  input(!noinput)
+  console.log("Entering password...")
+  }
+  return(
+    <div>
+      <input type="password" onChange={LoadingChange}></input>
+    </div>
+  )
+};
+
+export default Keypad;
